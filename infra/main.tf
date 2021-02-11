18ef0b2877d22d62cd6fd0bb8c2a98ccaf05350d
Ken Moini <ken@kenmoini.com>
2021-02-11 10:58:44 -0500

    push it

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,14 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-c"
 }
-
+variable "host_config_key" {
+  default = "123"
+  type = string
+}
+variable "tower_endpoint" {
+  default = "123"
+  type = string
+}
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
@@ -20,5 +27,6 @@ resource "google_compute_instance" "vm_instance" {
     access_config {
     }
   }
-  metadata_startup_script = "touch /opt/.remote-execd"
+  metadata_startup_script = format("useradd kemo && mkdir -p /home/kemo/.ssh && usermod -aG sudo kemo && echo \"%s\" > /home/kemo/.ssh/authorized_keys && curl --data \"host_config_key=%s\" %s -k",file("~/.ssh/id_rsa.pub"), var.host_config_key, var.tower_endpoint)
+
 }
