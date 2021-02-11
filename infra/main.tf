d36c263f7fc7586c063af3efb2ee41c7e5cf2f63
Ken Moini <ken@kenmoini.com>
2021-02-11 14:57:46 -0500

    combined api calls to script

--- infra/main.tf
+++ infra/main.tf
@@ -4,12 +4,24 @@ provider "google" {
   zone    = "us-central1-c"
 }
 variable "host_config_key" {
-  default = "123"
-  type = string
+  default = "1bc43655-2f11-4dd9-a8e1-2d72a56e4140"
+  type    = string
 }
 variable "tower_endpoint" {
+  default = "https://twr.example.com:443/api/v2/job_templates/11/callback/"
+  type    = string
+}
+variable "tower_server" {
+  default = "https://twr.example.com"
+  type    = string
+}
+variable "tower_job_id" {
+  default = 11
+  type    = number
+}
+variable "tower_inventory_id" {
+  type    = string
   default = "123"
-  type = string
 }
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
@@ -27,6 +39,7 @@ resource "google_compute_instance" "vm_instance" {
     access_config {
     }
   }
-  metadata_startup_script = format("useradd kemo && mkdir -p /home/kemo/.ssh && usermod -aG sudo kemo && echo \"%s\" > /home/kemo/.ssh/authorized_keys && curl --data \"host_config_key=%s\" %s -k",file("~/.ssh/id_rsa.pub"), var.host_config_key, var.tower_endpoint)
+  #metadata_startup_script = format("useradd kemo && mkdir -p /home/kemo/.ssh && usermod -aG sudo kemo && echo \"%s\" > /home/kemo/.ssh/authorized_keys && curl --data \"host_config_key=%s\" %s -k", file("~/.ssh/id_rsa.pub"), var.host_config_key, var.tower_endpoint)
+  metadata_startup_script = format("useradd kemo && mkdir -p /home/kemo/.ssh && usermod -aG sudo kemo && echo \"%s\" > /home/kemo/.ssh/authorized_keys && curl -sSL -O /opt/request_tower_configuration.sh https://raw.githubusercontent.com/kenmoini/terraform-tower-gcp-post-provision-callback-demo/main/configure-system.yaml && chmod +x /opt/request_tower_configuration.sh && /opt/request_tower_configuration.sh -k -c %s -s %s -t %d -i %d", file("~/.ssh/id_rsa.pub"), var.host_config_key, var.tower_server, var.tower_job_id, var.tower_inventory_id)
 
 }
