b87c0dac5cc329baf1cd5afffd6ad54e52a3b480
Ken Moini <ken@kenmoini.com>
2021-02-11 22:53:44 -0500

    remove haproxy stuff this is just a demo and cleanup terraform

--- infra/main.tf
+++ infra/main.tf
@@ -4,11 +4,7 @@ provider "google" {
   zone    = "us-central1-c"
 }
 variable "host_config_key" {
-  default = "1bc43655-2f11-4dd9-a8e1-2d72a56e4140"
-  type    = string
-}
-variable "tower_endpoint" {
-  default = "https://twr.example.com:443/api/v2/job_templates/11/callback/"
+  default = "1234567-4ef1-41a8-9454-2c57564e0f76"
   type    = string
 }
 variable "tower_server" {
@@ -16,20 +12,16 @@ variable "tower_server" {
   type    = string
 }
 variable "tower_job_id" {
-  default = 11
+  default = 123
   type    = number
 }
-variable "tower_inventory_id" {
-  type    = string
-  default = "123"
-}
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "terraform-rhel-instance"
   machine_type = "f1-micro"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "rhel-8-v20210122"
     }
   }
 
@@ -39,7 +31,6 @@ resource "google_compute_instance" "vm_instance" {
     access_config {
     }
   }
-  #metadata_startup_script = format("useradd kemo && mkdir -p /home/kemo/.ssh && usermod -aG sudo kemo && echo \"%s\" > /home/kemo/.ssh/authorized_keys && curl --data \"host_config_key=%s\" %s -k", file("~/.ssh/id_rsa.pub"), var.host_config_key, var.tower_endpoint)
-  metadata_startup_script = format("useradd kemo && mkdir -p /home/kemo/.ssh && usermod -aG sudo kemo && echo \"%s\" > /home/kemo/.ssh/authorized_keys && curl -sSL -O /opt/request_tower_configuration.sh https://raw.githubusercontent.com/kenmoini/terraform-tower-gcp-post-provision-callback-demo/main/request_tower_configuration.sh && chmod +x /opt/request_tower_configuration.sh && /opt/request_tower_configuration.sh -k -c %s -s %s -t %d -i %d", file("~/.ssh/id_rsa.pub"), var.host_config_key, var.tower_server, var.tower_job_id, var.tower_inventory_id)
+  metadata_startup_script = format("useradd nuser && mkdir -p /home/nuser/.ssh && usermod -aG wheel nuser && echo \"nuser ALL=(ALL) NOPASSWD: ALL\" > /etc/sudoers && echo \"%s\" > /home/nuser/.ssh/authorized_keys && chown -R nuser:nuser /home/nuser && curl -sSL https://raw.githubusercontent.com/kenmoini/terraform-tower-gcp-post-provision-callback-demo/main/request_tower_configuration.sh > /opt/request_tower_configuration.sh && chmod +x /opt/request_tower_configuration.sh && /opt/request_tower_configuration.sh -k -c %s -s %s -t %d", file("~/.ssh/id_rsa.pub"), var.host_config_key, var.tower_server, var.tower_job_id)
 
 }
